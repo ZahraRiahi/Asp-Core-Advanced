08efe8cbc23256cc2d4522ae297a959df566a5cf
ZahraRiahi <riahizahra67@gmail.com>
2023-07-22 23:48:27 +0430

    Excercise 12 comitted

--- Excercise 12/App/wwwroot/js/site.js
+++ Excercise 12/App/wwwroot/js/site.js
@@ -21,3 +21,45 @@
 		}
 	});
 });
+    document.addEventListener("DOMContentLoaded", function () {
+        const modal = document.getElementById("deleteProductModal");
+        const closeBtn = modal.querySelector(".close");
+        const confirmBtn = modal.getElementById("confirmDeleteBtn");
+        const cancelBtn = modal.getElementById("cancelDeleteBtn");
+        const deleteLinks = document.querySelectorAll(".delete");
+
+        function showModal() {
+            modal.style.display = "block";
+        }
+
+        function hideModal() {
+            modal.style.display = "none";
+        }
+
+        deleteLinks.forEach(link => {
+            link.addEventListener("click", function (event) {
+                event.preventDefault();
+                const itemId = this.getAttribute("data-id");
+                showModal();
+
+                confirmBtn.addEventListener("click", function () {
+                   
+                    hideModal();
+                });
+
+                cancelBtn.addEventListener("click", function () {
+                    hideModal();
+                });
+            });
+        });
+
+        closeBtn.addEventListener("click", function () {
+            hideModal();
+        });
+
+        window.addEventListener("click", function (event) {
+            if (event.target === modal) {
+                hideModal();
+            }
+        });
+    });
